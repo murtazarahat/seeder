b9335e3f06f3d285926b59a47daaa56c3f46a73f
Syed Rahat Murtaza <rahat_murtaza@outlook.com>
2023-07-17 08:28:34 +0500

    feat(test): added test for generate name func

--- seeder_test.go
+++ seeder_test.go
@@ -1,1 +1,19 @@
 package seeder
+
+import (
+	"testing"
+	"time"
+)
+
+func TestGenerateName(t *testing.T) {
+	seed := time.Now().UTC().UnixNano()
+	seeder := NewSeeder(seed)
+	if seeder == nil {
+		t.Errorf("seeder is null %v", seeder)
+	}
+
+	name := seeder.GenerateName()
+	if name == "" {
+		t.Error("could not generate name")
+	}
+}
